ce4d8f4cfc8b25e2bd477fb7364bcdc61dec7083
szyper <zskgit@gmail.com>
2023-04-01 18:30:49 +0200

    php

--- scripts/add_user.php
+++ scripts/add_user.php
@@ -1,4 +1,5 @@
 <?php
+	session_start();
 //	print_r($_POST);
 foreach ($_POST as $key => $value) {
 //	echo "$key: $value<br>";
@@ -10,3 +11,16 @@ foreach ($_POST as $key => $value) {
 	}
 }
 
+require_once "./connect.php";
+$sql = "INSERT INTO `users` (`city_id`, `firstName`, `lastName`, `birthday`) VALUES ('$_POST[city_id]', '$_POST[firstName]', '$_POST[lastName]', '$_POST[birthday]');";
+$conn->query($sql);
+
+if ($conn->affected_rows == 1){
+	$_SESSION['error'] = "Prawidłowo dodano rekord";
+}else{
+	$_SESSION['error'] = "Błędnie dodano rekord";
+}
+
+header("location: ../3_db/3_db_table_delete.php");
+
+//dokończyć sesję
